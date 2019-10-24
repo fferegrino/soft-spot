d3d65214303136fd11792fdc4a4f66522bbab31f
Antonio Feregrino <antonio.feregrino@gmail.com>
2019-10-24 07:44:37 +0100

    Restructure request code

--- soft_spot/implementations/request.py
+++ soft_spot/implementations/request.py
@@ -6,6 +6,12 @@ import click
 DELAY = 10
 
 
+class SpotRequestError(Exception):
+    def __init__(self, message, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.message = message
+
+
 def get_instance_configuration(config):
     instance = partial(config.get, "INSTANCE")
 
@@ -30,49 +36,57 @@ def wait_until_instance_ready(client, instance_id):
         response = client.describe_instance_status(InstanceIds=[instance_id])
         status = response["InstanceStatuses"][0]["InstanceState"]["Name"]
         sleep(1)
-    return response["InstanceStatuses"][0]["InstanceState"]["Name"]
+    return response["InstanceStatuses"][0]
 
 
-def request_instance(client, config):
-
-    instance_config = get_instance_configuration(config)
+def get_spot_request(client, instance_config):
     request_response = client.request_spot_instances(**instance_config)
     current_request = request_response["SpotInstanceRequests"][0]
     request_id = current_request["SpotInstanceRequestId"]
     click.echo(f"Spot request {request_id} created, status: {current_request['State']}")
     spot_request = wait_for_spot_request(client, request_id)
+    return spot_request
 
-    if spot_request["State"] == "active":
-        instance = get_instance_from(client, spot_request)
 
-        client.create_tags(
-            Resources=[spot_request["InstanceId"]],
-            Tags=[{"Key": "CreatedBy", "Value": "SoftSpot"}],
+def request_instance(client, config):
+    instance_config = get_instance_configuration(config)
+    spot_request = get_spot_request(client, instance_config)
+
+    if spot_request["State"] != "active":
+        raise SpotRequestError(
+            f"The spot request has failed, the state is {spot_request['State']}"
         )
 
-        instance_id = instance["InstanceId"]
-        public_ip = get_public_ip(instance)
-        click.echo(f"Instance {instance_id} started, IP: {public_ip}")
-        instance_status = wait_until_instance_ready(client, instance_id)
-        if instance_status == "running":
-            if config.has_section("VOLUME"):
-                attach_device(client, instance_id, config)
-            click.echo(
-                click.style(
-                    f"Done! the IP of the image is {public_ip}", bg="blue", fg="white"
-                )
-            )
-        else:
-            click.echo(
-                f"The instance created changed to a non-running state: {instance_status}"
-            )
-    else:
-        click.echo(f"The request failed, status {spot_request['State']}")
+    instance = get_instance_from(client, spot_request)
+    instance_state = instance["InstanceState"]["Name"]
+    if instance_state != "running":
+        raise SpotRequestError(
+            f"The request was fullified but the instance state is {instance_state}"
+        )
+
+    public_ip = get_public_ip(instance)
+
+    if config.has_section("VOLUME"):
+        attach_device(client, instance["InstanceId"], config)
+
+    click.echo(
+        click.style(f"Done! the IP of the image is {public_ip}", bg="blue", fg="white")
+    )
 
 
 def get_instance_from(client, spot_request):
     instances = client.describe_instances(InstanceIds=[spot_request["InstanceId"]])
-    return instances["Reservations"][0]["Instances"][0]
+    [instance] = instances["Reservations"][0]["Instances"]
+
+    client.create_tags(
+        Resources=[instance["InstanceId"]],
+        Tags=[{"Key": "CreatedBy", "Value": "SoftSpot"}],
+    )
+
+    instance_id = instance["InstanceId"]
+    public_ip = get_public_ip(instance)
+    click.echo(f"Instance {instance_id} started, IP: {public_ip}")
+    return wait_until_instance_ready(client, instance_id)
 
 
 def wait_for_spot_request(client, spot_request_id):
